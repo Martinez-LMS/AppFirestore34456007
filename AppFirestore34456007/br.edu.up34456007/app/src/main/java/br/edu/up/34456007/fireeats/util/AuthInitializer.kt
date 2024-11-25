cb648a935622f2bb70a525d6fd34c4b570843d0e
Martinez-LMS <leo.ooosla@gmail.com>
2024-11-24 21:25:47 -0300

    quase funcionando fire

--- AppFirestore34456007/br.edu.up34456007/app/src/main/java/br/edu/up/34456007/fireeats/util/AuthInitializer.kt
+++ AppFirestore34456007/br.edu.up34456007/app/src/main/java/br/edu/up/34456007/fireeats/util/AuthInitializer.kt
@@ -8,20 +8,19 @@ import com.google.firebase.example.fireeats.BuildConfig
 import com.google.firebase.ktx.Firebase
 
 class AuthInitializer : Initializer<FirebaseAuth> {
-    // The host '10.0.2.2' is a special IP address to let the
-    // Android emulator connect to 'localhost'.
+
     private val AUTH_EMULATOR_HOST = "10.0.2.2"
     private val AUTH_EMULATOR_PORT = 9099
 
     override fun create(context: Context): FirebaseAuth {
         val firebaseAuth = Firebase.auth
-        // Use emulators only in debug builds
+
         if (BuildConfig.DEBUG) {
             firebaseAuth.useEmulator(AUTH_EMULATOR_HOST, AUTH_EMULATOR_PORT)
         }
         return firebaseAuth
     }
 
-    // No dependencies on other libraries
+
     override fun dependencies(): MutableList<Class<out Initializer<*>>> = mutableListOf()
 }
